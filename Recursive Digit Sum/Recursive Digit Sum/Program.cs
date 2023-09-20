23a0f99bcace220a9aac04efa7c7b7dc6f916c71
Mjozsef <68011008+MJozsef22@users.noreply.github.com>
2023-09-20 10:37:10 +0200

    homework

---
+++ Recursive Digit Sum/Recursive Digit Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Digit_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "148";
+            int k = n.ToString().Length;
+
+            Console.WriteLine(superDigit(n,k));
+            Console.ReadKey();
+        }
+
+
+        public static int superDigit(string n,int k) 
+        {
+            if (n.Length == 1)
+            {
+                return int.Parse(n);
+            }
+
+            
+            double initialSum = 0;
+            foreach (char digit in n)
+            {
+                initialSum += (digit - '0'); 
+            }
+            initialSum *= k;
+
+            
+            return superDigit(initialSum.ToString(), 1);
+        }
+    }
+}
